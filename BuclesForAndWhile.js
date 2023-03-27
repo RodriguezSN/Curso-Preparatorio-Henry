f71ab8b6f1134558cfffbbb23a47fdde249b70a1
RodriguezSN <gamer.infante51@gmail.com>
2023-03-27 13:43:38 -0300

    practica

---
+++ BuclesForAndWhile.js
@@ -0,0 +1,17 @@
+var suma = 0;
+//suma = suma + 1;
+//suma = suma + 2;
+//suma = suma + 3;
+//suma = suma + 4;
+//console.log(suma);
+
+// for(var i=0 ; i<20 ; i++){
+//     suma = suma + i;
+//     console.log("variable de iteracion " , i);
+// }
+// console.log("variable suma ", suma)
+
+while (suma < 3){
+    suma = suma + 1;
+    console.log(suma);
+}
---
+++ switch.js
@@ -0,0 +1,27 @@
+var num = 3;
+
+// switch(num){
+//     case 1: console.log("num tiene el valor 1");
+//     break;
+//     case 2: console.log("num tiene el valor 2");
+//     break;
+//     case 3: console.log("num tiene el valor 3");
+//     break;
+//     case 4: console.log("num tiene el valor 4");
+//     break;
+//     case 5: console.log("num tiene el valor 5");
+//     break;
+//     default: console.log("num no vale ninguno de estos numeros")
+// }
+
+
+// switch(num){
+//     case 1:
+//     case 3:
+//     case 5:
+//         console.log("num es impar");
+//     break;
+//     case 2:
+//     case 4:
+//         console.log("num es par");
+// }
