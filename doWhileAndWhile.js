043d92644985000007f6fffd7621f753afeee129
RodriguezSN <gamer.infante51@gmail.com>
2023-03-27 21:02:22 -0300

    practica

---
+++ doWhileAndWhile.js
@@ -0,0 +1,10 @@
+var password = "";
+
+while (password != "hola"){
+    password = prompt("intente nuevamente")
+} 
+console.log("fin del bucle")
+
+// do {
+//     password = pro ("intente nuevamente")
+// }while (password != "hola")
--- switch.js
+++ switch.js
@@ -1,4 +1,4 @@
-var num = 3;
+// var num = 3;
 
 // switch(num){
 //     case 1: console.log("num tiene el valor 1");
@@ -25,3 +25,28 @@ var num = 3;
 //     case 4:
 //         console.log("num es par");
 // }
+
+var producto = "banana"
+
+switch(producto){
+    case "banana":
+    console.log("el kl de bananan esta 120$")
+    break
+    case "1/2 de banana":
+    console.log("son 60 pe master ")
+    break
+    case "tomate":
+    console.log("el kl de tomate esta 130$")
+    break
+    case "manzana":
+    console.log("el kl de manzana esta 90$")
+    break
+    case "naranja":
+    console.log("el kl de naranja esta 150$")
+    break
+    default:
+    console.log("Lo lamentamos, por el momento no tenemos " + producto + ".")
+}
+
+console.log("hay algo mas que desea consultar?")
+
